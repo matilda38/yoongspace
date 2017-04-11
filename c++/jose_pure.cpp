c8ff3c562cbc906820f9f5fd15e7558f6fb4d672
matilda38 <matilda38@naver.com>
2017-04-12 00:20:01 +0900

    with/without vector josepus queue

---
+++ c++/jose_pure.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(){
+  int n , m, i;
+  cin >> n;
+  cin >> m;
+
+  queue<int> q;
+  //initalize
+  for(i=0;i<n;i++){
+    q.push(i+1);
+  }
+
+  n--;
+  cout << "<"
+  while(n--){
+    for(i=0;i<m;i++){
+      q.push(q.front());
+      q.pop();
+    }
+    cout << q.front();
+    q.pop();
+    cout << ", ";
+  }
+  cout << q.front();
+  cout << ">\n";
+
+  return 0;
+}
